3a09b6b4bfaa07a6d5edaec365ccf8a51f4c8175
64blit <61922101+64blit@users.noreply.github.com>
2023-08-11 08:09:56 -0700

    Update build.ps1

--- devops/build.ps1
+++ devops/build.ps1
@@ -5,16 +5,20 @@ $build_path = "D:\_SPACE\Web\xyz3d.r3f\dist"
 $src_path = "D:\_SPACE\Web\xyz3d.r3f\"
 
 # Set the destination paths
-$preview_dest = "D:\_SPACE\Blender\WebCrafterPro-blender-plugin\Templates\scroll\preview"
-$r3f_dest = "D:\_SPACE\Blender\WebCrafterPro-blender-plugin\Templates\scroll\source"
+$preview_dest = "D:\_SPACE\Blender\WebCrafterPro-blender-plugin\Templates\orbit\preview"
+$r3f_dest = "D:\_SPACE\Blender\WebCrafterPro-blender-plugin\Templates\orbit\source"
 
 # Build the project using yarn
 Write-Host "Building the project..."
 
 Write-Host $build_path
 Write-Host $preview_dest
-
+# Set debugging state to true
+$env:REACT_APP_DEBUG = "true"
 yarn build
+
+# Set debugging state to false after build
+$env:REACT_APP_DEBUG = "false"
 
 # Copy the dist folder contents to the preview destination
 Write-Host "Copying dist folder contents to preview destination..."
