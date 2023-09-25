5411cbae189458c3da9a696cd15013b766d6520c
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-09-25 15:12:12 -0700

    Updated overlay to have loading screen and arrow back

--- src/components/HtmlOverlay.jsx
+++ src/components/HtmlOverlay.jsx
@@ -1,20 +1,44 @@
+import { Suspense, useEffect, useRef, useState } from "react";
 
 export const HtmlOverlay = (props) =>
 {
+    const [ loading, setLoading ] = useState(true);
+    const iframeRef = useRef();
+
+    useEffect(() =>
+    {
+        try
+        {
+            const iframe = iframeRef.current;
+            iframe.onload = () =>
+            {
+                setLoading(false);
+            };
+        } catch (error)
+        {
+            setLoading(false);
+        }
+    }, [ iframeRef ]);
+
 
     return (
-        <div className="fixed top-0 left-0 flex flex-col items-center justify-center w-full h-full bg-black">
-
-            <button className="absolute top-2 right-2 p-0.5 mb-2 mr-2 overflow-hidden h-[2.5rem]  text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-                onClick={() => { props.setShowPopup(false) }}>
-                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    X
+        <div className="absolute top-0 left-0 w-screen h-screen bg-black">
+            <button className="absolute top-0 left-0 h-[2.5rem] w-[2.5rem] text-gray-900 group  hover:text-white dark:text-white focus:ring-4 focus:outline-none"
+                onClick={() => { props.setShowPopup(false); setLoading(true); }}>
+                <span className="relative -top-4 -left-2 pl-2 transition-all text-7xl ease-in duration-75 bg-white group-hover:bg-opacity-0 rounded-r-3xl">
+                    ⇠
                 </span>
             </button>
 
+            <div className=" w-full h-full flex flex-col justify-center items-center">
 
-            <iframe src={props.content} className="w-full h-full"></iframe>
+                {
+                    loading ?
+                        <div className="bg-white text-black text-9xl ">Loading...</div>
+                        : <iframe ref={iframeRef} src={props.content} className="w-full h-full"></iframe>
+                }
 
+            </div>
         </div>
     );
 };
