efb6541d630833bd833fae50d9427ebf829ccbc0
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-09-25 14:35:46 -0700

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,12 @@
 import { Html } from '@react-three/drei';
 import React, { useEffect, useState } from 'react';
 
+const generateKey = (pre) =>
+{
+    return `${pre}_${new Date().getTime()}`;
+}
+
+
 export function NavBar(props)
 {
     const [ sceneManagerInitialized, setSceneManagerInitialized ] = useState(false);
@@ -49,27 +55,27 @@ export function NavBar(props)
                     <ul className="hidden md:flex items-center text-[18px] font-semibold">
                         {sceneManagerInitialized &&
                             sceneZones?.map((sceneZone, index) => (
-                                <li onClick={navigateTo(sceneZone.name)} key={index} className="text-white  transition duration-1000 ease-in-out border-white hover:italic hover:underline hover:scale-125 mx-4 my-1 bg-black pl-4 pr-4 hover:cursor-pointer hover:text-blue-300"><a
+                                <li onClick={navigateTo(sceneZone.name)} key={generateKey(index)} className="text-white  transition duration-1000 ease-in-out border-white hover:italic hover:underline hover:scale-125 mx-4 my-1 bg-black pl-4 pr-4 hover:cursor-pointer hover:text-blue-300"><a
                                     href="#"> {sceneZone.name}</a></li>
                             ))}
                     </ul>
 
-                    <button className="block p-3 mx-10 md:hidden transition duration-500 ease-in-out  hover:bg-gray-400 rounded group bg-gray-200">
-                        <div className="z-20 w-5 my-[3px] h-[3px] bg-gray-600 mb-[2px]"></div>
-                        <div className="z-20 w-5 my-[3px] h-[3px] bg-gray-600 mb-[2px]"></div>
-                        <div className="z-20 w-5 my-[3px] h-[3px] bg-gray-600"></div>
+                    <button className="block p-3 mx-10 md:hidden transition duration-500 ease-in-out  hover:bg-gray-200 rounded group bg-white">
+                        <div className="z-20 w-5 my-[3px] h-[3px] bg-gray-600 mb-[2px] rounded "></div>
+                        <div className="z-20 w-5 my-[3px] h-[3px] bg-gray-600 mb-[2px] rounded "></div>
+                        <div className="z-20 w-5 my-[3px] h-[3px] bg-gray-600 rounded "></div>
                         <div
                             className="transition-all ease-in duration-500 absolute top-0 -right-full opacity-0 w-full border bg-white group-focus:right-0 group-focus:opacity-100 h-screen">
                             <ul className="flex flex-col justify-center items-center text-[18px] pt-[1rem] min-w[10rem] h-screen">
                                 <a href="#">
-                                    <li onClick={(e) => { console.log(e); e.target.focus() }} className="text-stone-600 p-5 transition duration-500 ease-in-out  hover:bg-black hover:text-white hover:scale-125 font-medium mx-4 my-1 hover:underline hover:italic rounded-full w-[4rem] h-[4rem]"> X
+                                    <li onClick={(e) => { console.log(e); e.target.focus() }} key={generateKey("X")} className="text-stone-600 p-5 transition duration-500 ease-in-out  hover:bg-black hover:text-white hover:scale-125 font-medium mx-4 my-1 hover:underline hover:italic rounded-full w-[4rem] h-[4rem]"> X
                                     </li>
                                 </a>
 
                                 {sceneManagerInitialized &&
                                     sceneZones?.map((sceneZone, index) => (
-                                        <a href="#">
-                                            <li onClick={navigateTo(sceneZone.name)} key={index} className="text-stone-600 p-5 transition duration-500 ease-in-out  hover:bg-black hover:text-white hover:scale-125 font-medium mx-4 my-1 hover:underline hover:italic"> {sceneZone.name}
+                                        <a href="#" key={generateKey(index)}>
+                                            <li onClick={navigateTo(sceneZone.name)}  className="text-stone-600 p-5 transition duration-500 ease-in-out  hover:bg-black hover:text-white hover:scale-125 font-medium mx-4 my-1 hover:underline hover:italic"> {sceneZone.name}
                                             </li>
                                         </a>
                                     ))}
