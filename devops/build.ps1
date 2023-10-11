74a35c04fa7882ac1d0345e7ef3fab5cf607a15b
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-10-11 00:33:56 -0700

    updated building

--- devops/build.ps1
+++ devops/build.ps1
@@ -21,7 +21,7 @@ Remove-Item -Path $r3f_dest\* -Recurse -Force
 
 # run yarn build from the working directory of C:\Users\edmun\OneDrive\Documents\_SPACE\Web\xyz3d.r3f\
 cd "C:\Users\edmun\OneDrive\Documents\_SPACE\Web\xyz3d.r3f\"
-yarn build
+yarn exportit
 
 # Copy the dist folder contents to the preview destination
 Write-Host "Copying dist folder contents to preview destination..."
