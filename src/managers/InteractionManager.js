7d6b59e9d7e687de5e52fee299f9254b0e946c47
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-11-23 13:08:10 -0800

    Added sound playback on select

--- src/managers/InteractionManager.js
+++ src/managers/InteractionManager.js
@@ -17,10 +17,17 @@ export class InteractionManager
             const type = event.object.userData.interactableType;
             const data = event.object.userData.interactableData;
 
+            const sound = event.object.userData.mediaSrc || null;
+
+            if (sound != null)
+            {
+                this.playSound(sound);
+            }
+
             // Play all the select animations first, then trigger the interaction
             await this.handlePointerSelect(event.object);
 
-            document.body.style.cursor = "auto";
+            document.body.style.cursor = "pointer";
 
             switch (type)
             {
