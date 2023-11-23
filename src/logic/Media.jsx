66abc8976bcc9af87003c7e7aec5ce3f1107b6aa
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-11-23 13:05:35 -0800

    Cleaned up keys

--- src/logic/Media.jsx
+++ src/logic/Media.jsx
@@ -1,45 +1,46 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import { Audio3D } from './Audio3D.tsx';
 import { Video3D } from './Video3D.tsx';
+import { generateKey } from 'helpers/ReactHelpers.js';
 
-export const Media = (props = { sceneManager }) =>
+export const Media = ({ sceneManager, isDebugging }) =>
 {
     const [ mediaContent, setMediaContent ] = useState(null);
 
     useEffect(() =>
     {
-        if (!props.sceneManager) return;
-        if (!props.sceneManager.mediaObjects) return;
+        // if (!sceneManager) return;
+        // if (!sceneManager.mediaObjects) return;
 
         const loadMedia = (event) =>
         {
             const tempMediaContent =
                 <>
                     <group>
-                        {props.sceneManager?.mediaObjects.video.map((element, key) => (
+                        {sceneManager?.mediaObjects.video.map((element, key) => (
                             <Video3D
-                                key={key}
+                                key={generateKey(key)}
                                 size={Math.max(...element.object.scale)}
                                 src={element.mediaSrc}
                                 position={element.worldPosition}
                                 quaternion={element.worldRotation}
                                 sourceObject={element.object}
                                 muted={false}
+                                isDebugging={isDebugging}
                                 framed >
                             </Video3D>
                         ))}
-                    </group>
-                    <group>
-                        {props.sceneManager?.mediaObjects.positionalAudio.map((element, key) => (
+                        {sceneManager?.mediaObjects.positionalAudio.map((element, key) => (
                             <Audio3D
-                                key={key}
+                                key={generateKey(key)}
                                 size={Math.max(...element.object.scale)}
                                 url={element.mediaSrc}
                                 sourceObject={element.object}
                                 position={element.worldPosition}
                                 quaternion={element.worldRotation}
                                 muted={false}
+                                isDebugging={isDebugging}
                             />
                         ))}
                     </group>
@@ -57,7 +58,7 @@ export const Media = (props = { sceneManager }) =>
             document.removeEventListener("click", loadMedia);
         };
 
-    }, [ props.sceneManager ]);
+    }, [ sceneManager ]);
 
     return (
         <>
