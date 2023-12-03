4e3b157f0b0f35735a380385dcb8a03369e64a3b
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-12-02 18:01:16 -0800

    Update build_all.ps1

--- devops/build_all.ps1
+++ devops/build_all.ps1
@@ -1,23 +1,21 @@
 # A script that runs ./build.ps1 then changes branches and runs ./build.ps1 again
 #  Branches to build include: template-scroll, template-orbit, template-walkvr in that order
 
-# Run build.ps1
-./build.ps1
-
 # Change to template-scroll branch
-git checkout template-scroll
+git.exe checkout template-scroll
 
-# Run build.ps1
-./build.ps1
+# Run build.ps1 with powershell
+powershell.exe ./build.ps1
 
 # Change to template-orbit branch
-git checkout template-orbit
+git.exe checkout template-orbit
 
 # Run build.ps1
-./build.ps1
+powershell.exe ./build.ps1
+
 
 # Change to template-walkvr branch
-git checkout template-walkvr
+git.exe checkout template-walkvr
 
 # Run build.ps1
-./build.ps1
+powershell.exe ./build.ps1
